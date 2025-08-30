074877d038acdd5ef6c1266b60287849f53f460d
Stuartmedgar <stuartmedgar@gmail.com>
2025-08-30 21:14:37 +0100

    Create index.ts

---
+++ src/components/subscription/index.ts
@@ -0,0 +1,4 @@
+// Barrel export for subscription components - matches your existing pattern
+export { default as PricingPage } from './PricingPage';
+export { default as MobilePricingPage } from './MobilePricingPage';
+export { default as SubscriptionSuccess } from './SubscriptionSuccess';
