6734ce0c8d5a3a4698a2aa9452a1a45dce596021
Stuart Edgar <stuartmedgar@gmail.com>
2026-05-19 21:36:25 +0100

    Fix Need a Template navigation

--- src/pages/Step2Template.tsx
+++ src/pages/Step2Template.tsx
@@ -73,8 +73,8 @@ function Step2Template() {
         maxWidth: '580px'
       }}>
 
-        {/* Need a Template → template wizard */}
-        <Link to="/import-template" style={{ textDecoration: 'none' }}>
+        {/* Need a Template → create template choice page */}
+        <Link to="/create-template" style={{ textDecoration: 'none' }}>
           <div
             style={{
               backgroundColor: '#8b5cf6', color: 'white',
